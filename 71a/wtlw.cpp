7023532a73ba82d9e70597f5be3307ee9b733f7c
SamPettus <spettus@umail.ucsb.edu>
2025-07-07 19:53:50 -0700

    71a code force first challenge

---
+++ 71a/wtlw.cpp
@@ -0,0 +1,92 @@
+/*
+Print n lines. The i-th line should contain the result of replacing of the i-th word from the input data.
+
+Examples
+InputCopy
+4
+word
+localization
+internationalization
+pneumonoultramicroscopicsilicovolcanoconiosis
+OutputCopy
+word
+l10n
+i18n
+p43s
+
+
+Basically, if word is longer than 10 letters, convert it to abbrevaition. 
+To do this, just take the first and last letter, use these as the current key
+in a dict. Starting at 0
+
+Using template found at: https://ncduy0303.github.io/Competitive-Programming/Contest%20Template/main.cpp
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string word;
+        cin >> word;
+        
+        string result;
+        if (word.size() > 10) {
+            result = word[0] + to_string(word.size() - 2) + word[word.size() - 1];
+        } else{
+            result = word;
+        }
+        
+        cout <<result<< "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
